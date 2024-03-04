18c8e380a5ec024e997f4de04a83bb9b7a4847af
YounessABBOUABDELLAH <149901593+YounessABBOUABDELLAH@users.noreply.github.com>
2024-03-04 21:46:09 +0100

    Update fichierInfo.java

--- src/main/java/entities/fichierInfo.java
+++ src/main/java/entities/fichierInfo.java
@@ -11,22 +11,5 @@ import lombok.NoArgsConstructor;
 public class fichierInfo {
 	  private String filename;
 	    private String url;
-		public fichierInfo(String filename, String url) {
-			super();
-			this.filename = filename;
-			this.url = url;
-		}
-		public String getFilename() {
-			return filename;
-		}
-		public void setFilename(String filename) {
-			this.filename = filename;
-		}
-		public String getUrl() {
-			return url;
-		}
-		public void setUrl(String url) {
-			this.url = url;
-		}
 	
 }
